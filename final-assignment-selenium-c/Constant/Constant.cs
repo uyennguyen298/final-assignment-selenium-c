78d9f756b5064b00c06b7fd3375cb6147adcc1a7
uyennguyen298 <diemuyen.nguyen298@gmail.com>
2022-04-26 01:08:06 +1000

    Add project files.

---
+++ final-assignment-selenium-c/Constant/Constant.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace final_assignment_selenium_c.Constant
+{
+    class Constant
+    {
+        public static IWebDriver WEBDRIVER;
+        public static String APP_URL = "http://automationpractice.com/index.php";
+    }
+}
